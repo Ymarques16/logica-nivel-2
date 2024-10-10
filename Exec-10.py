d67d82a125a433a4ab20f1ecbee08b51386c60ce
Ymarques16 <yagorodrigues38@gmail.com>
2024-10-10 20:46:09 -0300

    Create Exec-10.py

---
+++ Exec-10.py
@@ -0,0 +1,13 @@
+quantidade_vendida = int(input("Quantos carros foram vendidos pelo funcionario?"))
+valor_vendido = int(input("quanto o funcionario vendeu?"))
+
+salario_fixo = 1500
+
+comissao = (quantidade_vendida*700)
+
+comissao_do_valor_vendido = 0
+
+salario_total = (salario_fixo + comissao)
+
+print("O salário total será:", salario_total)
+print("A comissão será:", valor_vendido  85)
