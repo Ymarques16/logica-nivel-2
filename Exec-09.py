b2fc2a8fbd1d2670e3404dc789a1829ec500ccf2
Ymarques16 <yagorodrigues38@gmail.com>
2024-10-10 19:15:42 -0300

    Update Exec-09.py

--- Exec-09.py
+++ Exec-09.py
@@ -1,6 +1,6 @@
 valor_carro = int(input("qual é o valor de fabrica do carro?"))
 percentual_distribuidor = 28
 imposto = 45
-valor_total = (valor_carro*1.28) * (1.45)
+valor_total = (valor_carro*percentual_distribuidor) * (imposto)
 
 print("o valor completo do carro é:", valor_total)
