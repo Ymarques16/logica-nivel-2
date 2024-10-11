a091286402de739fec2ecba977955a9a83fef2ab
Ymarques16 <yagorodrigues38@gmail.com>
2024-10-10 21:04:23 -0300

    Create Exec-12.py

---
+++ Exec-12.py
@@ -0,0 +1,9 @@
+nota1 = int(input("Informe qual a nota do 1° bimestre?"))
+
+nota2 = int(input("Informe qual a nota do 2° bimestre"))
+
+nota3 = int(input("Informe qual a nota do 3° bimestre"))
+
+média_final = (nota1 + nota2 + nota3) / 3
+
+print("A média é:", média_final)
