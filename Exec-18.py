e4a80fb48dcb4a56bc0d7e45ed2fed77dc255c39
Ymarques16 <yagorodrigues38@gmail.com>
2024-10-17 19:50:26 -0300

    Create Exec-18.py

---
+++ Exec-18.py
@@ -0,0 +1,11 @@
+valor1 = int(input("Digite um valor:"))
+valor2 = int(input("Digite outro valor:"))
+
+
+if valor1 > valor2:
+    print(f"{valor1} é maior que {valor2}")
+elif valor1 < valor2:
+    print(f"{valor1} é menor que {valor2}")
+else: 
+     print("Os valores são iguais.") 
+    
