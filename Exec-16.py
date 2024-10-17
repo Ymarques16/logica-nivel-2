ce246671ca6607b47c8474c5653088bb16aa23ae
Ymarques16 <yagorodrigues38@gmail.com>
2024-10-17 19:12:20 -0300

    Create Exec-16.py

---
+++ Exec-16.py
@@ -0,0 +1,12 @@
+nota1 = int(input("Informe qual a nota da 1° prova:"))
+
+nota2 = int(input("Informe qual a nota da 2° prova:"))
+
+média = (nota1 + nota2 ) / 2
+
+if média >= 6:
+    
+    print("O aluno foi aprovado!")
+else:
+    
+    print("O aluno foi reprovado!")
