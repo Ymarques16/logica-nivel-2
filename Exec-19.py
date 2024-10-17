b09f61a2671dc3e6ac69acd3c7d3f3c53d583c99
Ymarques16 <yagorodrigues38@gmail.com>
2024-10-17 20:22:09 -0300

    Create Exec-19.py

---
+++ Exec-19.py
@@ -0,0 +1,7 @@
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+if numero1 < numero2:
+    print(f"Ordem crescente: {numero1}, {numero2}")
+else:
+    print(f"Ordem crescente: {numero2}, {numero1}")
