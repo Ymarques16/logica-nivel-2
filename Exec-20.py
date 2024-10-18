d79facf756a1860d9437b309fa4d080438558b71
Ymarques16 <yagorodrigues38@gmail.com>
2024-10-17 21:19:02 -0300

    Update Exec-20.py

--- Exec-20.py
+++ Exec-20.py
@@ -1,13 +1,9 @@
-horaInicio = 22
-horaFinal = 1
+horaInicio = int(input("Em que horário a partida iniciou?"))
+horaFinal = int(input("Em que horário a partida terminou?"))
 
 if horaFinal < horaInicio:
     horaParcial1 = 24 - horaInicio
     resultado = horaParcial1 + horaFinal
     print(resultado)
 else:
-   print(horaFinal-horaInicio)
-    
-    print("começou e terminou no mesmo dia")
-
-
+   print("A partida durou:", horaFinal-horaInicio)
