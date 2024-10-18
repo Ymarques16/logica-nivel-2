2ba87e2016d256e09c9f75b06122b06b3aeafefe
Ymarques16 <yagorodrigues38@gmail.com>
2024-10-18 20:17:17 -0300

    Update Exec-21.py

--- Exec-21.py
+++ Exec-21.py
@@ -1,1 +1,11 @@
+horas = int(input("quantas horas regulares foram trabalhadas?"))
+horasExtra = int(input("quantas horas extra foram trabalhadas?"))
+horasDeTrabalho = horas
+valorPorHora =35.45
+extra = 17.725*horasExtra
 
+if horasExtra >= 1:
+    resultado = valorPorHora*horas+extra
+    print("O salário atual será:", resultado)
+else:
+    print("o salário total será:", valorPorHora*horasDeTrabalho)
