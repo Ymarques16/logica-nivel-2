61130df6d5f27096145ebffb81f18651617ae00c
Ymarques16 <yagorodrigues38@gmail.com>
2024-10-17 21:01:34 -0300

    Create Exec-20.py

---
+++ Exec-20.py
@@ -0,0 +1,13 @@
+horaInicio = 22
+horaFinal = 1
+
+if horaFinal < horaInicio:
+    horaParcial1 = 24 - horaInicio
+    resultado = horaParcial1 + horaFinal
+    print(resultado)
+else:
+   print(horaFinal-horaInicio)
+    
+    print("começou e terminou no mesmo dia")
+
+
