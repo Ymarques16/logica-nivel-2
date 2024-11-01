c6353784c5e61731b68dab7731746c168381b51c
Ymarques16 <yagorodrigues38@gmail.com>
2024-11-01 20:25:22 -0300

    Update Exec-23.py

--- Exec-23.py
+++ Exec-23.py
@@ -1,1 +1,11 @@
+TotalDeVendas = float(input("qual foi o valor das vendas?"))
+comisssao1 = TotalDeVendas*0.03
+comissao2 = TotalDeVendas*0.05
+
+if TotalDeVendas >=1500:
+   print(1500+comisssao1)
+ 
+else:
+  print(1500+comissao2)
+    
 
