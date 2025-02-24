b08ab890be31ed777b66ae7a5dff4a18cb418976
Ymarques16 <yagorodrigues38@gmail.com>
2025-02-24 19:27:51 -0300

    Update Exec-07.py

--- Exec-07.py
+++ Exec-07.py
@@ -1,6 +1,6 @@
 anos = int(input("digite a idade em anos"))
-anos = int(input("digite a idade em meses"))
-
-dias_totais = (anos*365) + (meses*30)
+meses = int(input("digite a idade em meses"))
+dias = int(input("digite a idade em dias"))
+dias_totais = (anos*365) + (meses*30) + (dias)
 
 print("seus dias de idade são:", dias_totais)
